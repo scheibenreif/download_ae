0923c2fc3536d9d29e0df3f79fe8a251ddd0cdca
Linus Scheibenreif <lscheibenrei@ethz.ch>
2026-02-10 10:35:02 +0100

    Update example script.

--- example_usage.py
+++ example_usage.py
@@ -2,27 +2,32 @@
 Simple usage example for Alpha Earth downloader.
 """
 
-import numpy as np
 import tqdm
 import pandas as pd
 
 from ae_downloader import Downloader
 
 
-year = 2022
-locations = {"san_francisco": (37.7749, -122.4194),
-             "new_york": (40.7128, -74.0060),
-             "los_angeles": (34.0522, -118.2437),
-             "bregenz": (47.50489881092864, 9.725558000922499),
-            }
+year = 2022  # Use embeddings from this year.
+tile_size = 5  # Height and width of the downloaded data (10m GSD).
 
+# Load the locations of air pollution monitoring stations.
 station_data = pd.read_csv("../global_air_pollution_datalake/station_data.csv")
-downloader = Downloader(output_path="./ae_embeddings/air_pollution/", tile_size=5)
 
-# for location_id, (lat, lon) in tqdm.tqdm(locations.items()):
-for idx, (location_id, lat, lon) in tqdm.tqdm(station_data.iterrows(), total=station_data.shape[0]):
-    try:
-        downloader.download(lat, lon, year, location_id)
-    except Exception as e:
-        print(e)
-        continue
+# Use some sample locations.
+# locations = {"san_francisco": (37.7749, -122.4194),
+#              "new_york": (40.7128, -74.0060),
+#              "los_angeles": (34.0522, -118.2437),
+#              "bregenz": (47.5049, 9.7256),
+#             }
+
+if __name__ == "__main__":
+    downloader = Downloader(output_path="./ae_embeddings/air_pollution/", tile_size=tile_size)
+
+    # for location_id, (lat, lon) in tqdm.tqdm(locations.items()):
+    for idx, (location_id, lat, lon) in tqdm.tqdm(station_data.iterrows(), total=station_data.shape[0]):
+        try:
+            downloader.download(lat, lon, year, location_id)
+        except Exception as e:
+            print(e)
+            continue
